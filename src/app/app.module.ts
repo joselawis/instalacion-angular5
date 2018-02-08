86173b38f9e8048be59d29b67b183c6e9c939d7c
Jose Luís Contreras Martínez <jcontrem@everis.com>
2018-02-08 08:47:00 +0100

    Primer componente: VideojuegosComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { VideojuegosComponent } from './videojuegos/videojuegos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideojuegosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/videojuegos/videojuegos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'videojuegos',
+    template: `
+        <h2>Componente de {{nombre}}</h2>
+        <h3>El mejor juego es: {{mejor_juego}}</h3>
+    `
+})
+export class VideojuegosComponent{
+    public nombre = 'Videojuegos 2018';
+    public mejor_juego = 'Monster Hunter World';
+}
